ed37399352b320e824e62d2bb9843f030afb6545
Doğanay Balaban <balabandoganay@gmail.com>
2025-11-19 21:23:04 +0300

    Add MongoDB connection and bcryptjs dependency
    Introduced a new db.ts config file to handle MongoDB connection using mongoose and dotenv. Updated index.ts to connect to the database on server start. Added bcryptjs and its type definitions to package.json for password hashing support.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { connectDB } from "./config/db";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
+    connectDB();
 })
